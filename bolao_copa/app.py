8d9db36672f88fcf374b9db947cee73b2b641baf
pretzel-z <petersonzanel725@gmail.com>
2026-06-17 01:57:21 -0300

    Update app.py

--- bolao_copa/app.py
+++ bolao_copa/app.py
@@ -16,7 +16,8 @@ resultados_reais = {
     7: {"gols_a": 0, "gols_b": 1}, 8: {"gols_a": 2, "gols_b": 0}, 9: {"gols_a": 7, "gols_b": 1},
     10: {"gols_a": 2, "gols_b": 2}, 11: {"gols_a": 1, "gols_b": 0}, 12: {"gols_a": 5, "gols_b": 1}, 
     13: {"gols_a": 0, "gols_b": 0}, 14: {"gols_a": 1, "gols_b": 1}, 15: {"gols_a": 1, "gols_b": 1}, 
-    16: {"gols_a": 2, "gols_b": 2}
+    16: {"gols_a": 2, "gols_b": 2}, 17: {"gols_a": 3, "gols_b": 1}, 18: {"gols_a": 1, "gols_b": 4}, 
+    19: {"gols_a": 3, "gols_b": 0}
 }
 
 jogos_grupos = [
